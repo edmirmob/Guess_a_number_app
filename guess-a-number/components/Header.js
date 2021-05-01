8b85acd052f2db78e7652ed77970f61ad96b62d9
Edmir Čolo <79378369+edmirmob@users.noreply.github.com>
2021-05-02 01:24:30 +0200

    Adding a Screen Component

--- guess-a-number/components/Header.js
+++ guess-a-number/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 const Header = props => {
-   return (
+    return (
         <View style={styles.header}>
             <Text style={styles.headerTitle}>{props.title}</Text>
         </View>
