eeaa1a04fec65d45da3c1825d1def0973b9c7939
Edmir Čolo <79378369+edmirmob@users.noreply.github.com>
2021-05-02 16:27:02 +0200

    Configuring & Styling a TextInput

--- guess-a-number/screens/StartGameScreen.js
+++ guess-a-number/screens/StartGameScreen.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 import Card from '../components/Card';
 import Colors from '../constants/Colors';
+import Input from '../components/Input';
 
 const StartGameScreen = props => {
     return (
@@ -9,7 +10,13 @@ const StartGameScreen = props => {
             <Text>Start a New Game</Text>
             <Card style={styles.inputContainer}>
                 <Text style={styles.title}>Select a Number</Text>
-                <TextInput />
+                <Input
+                    styles={styles.input}
+                    blurOnSubmit
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    keyboardType="numeric"
+                    maxLength={2} />
                 <View style={styles.buttonContainer}>
                     <View style={styles.button}><Button title='Reset' onPress={() => { }} color={Colors.accent} /></View>
                     <View style={styles.button}><Button title='Confirm' onPress={() => { }} color={Colors.primary} /></View>
@@ -22,13 +29,17 @@ const styles = StyleSheet.create({
     screen: {
         flex: 1,
         padding: 10,
-        alignItems: 'center'
+        alignItems: "center"
     },
     inputContainer: {
         width: 300,
         maxWidth: '80%',
-        alignItems: 'center',
+        alignItems: "center",
         marginTop: 10
+    },
+    input: {
+        width: 50,
+        textAlign: "center",
     },
     button: {
         flex: 1,
